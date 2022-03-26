1002b00eb3d35d1ad0a6217eac0bb84db6c985f5
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-03-26 18:45:32 +0900

    letter_opener_web をインストールし、開発環境でパスワード再設定のメールアドレスを取得、パスワード再設定の動作確認

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # デフォルトのURL
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :letter_opener_web
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index', as: 'users'
   get '/users/:id', to: 'users#show', as: 'user'
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
+
