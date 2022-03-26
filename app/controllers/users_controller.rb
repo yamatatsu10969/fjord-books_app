ba3b2514fc6f5db8e1b69626e3fb3861627a7a4d
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-03-26 17:50:27 +0900

    ユーザ一覧を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def index
+    @users = User.order(:id).page(params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get '/users/:id', to: 'users#show', as: 'user'
   devise_for :users
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
