b79e20b7422f34a280356c03cafa37be8ad4117f
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-03-26 16:54:02 +0900

    郵便番号・住所・自己紹介文を、ユーザ作成・編集で送信できるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    permit_param_keys = %i[introduction postal_code address]
+    devise_parameter_sanitizer.permit(:sign_up, keys: permit_param_keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: permit_param_keys)
+  end
 end
