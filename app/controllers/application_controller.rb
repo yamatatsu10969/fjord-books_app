daf9014ec023bbde74536da2b184b550e6c79338
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-03-26 17:06:34 +0900

    ログイン後は books#index、ログアウト後は devise/sessions#new に移動させる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: permit_param_keys)
     devise_parameter_sanitizer.permit(:account_update, keys: permit_param_keys)
   end
+
+  def after_sign_in_path_for(_)
+    books_path
+  end
+
+  def after_sign_out_path_for(_)
+    new_user_session_path
+  end
 end
