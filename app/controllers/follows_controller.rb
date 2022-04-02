df0889650568b00ad2edfe20fa2ee2bddcae76e4
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-04-02 11:26:19 +0900

    follows_controller.rbを作成し、createとdestroyメソッドを作成。routesにそれぞれのパスを追加

---
+++ app/controllers/follows_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class FollowsController < ApplicationController
+  def create
+    followee_id = params[:followee_id]
+    @follow = Follow.new(followee_id: followee_id, follower_id: current_user.id)
+    @follow.save
+    redirect_to user_path(followee_id)
+  end
+
+  def destroy
+    @follow = Follow.find(params[:id])
+    followee_id = @follow.followee_id
+    @follow.destroy
+    redirect_to user_path(followee_id)
+  end
+end
