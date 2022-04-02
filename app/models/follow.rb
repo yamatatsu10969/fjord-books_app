592fb9b09436d0aa592e0c2e066e880638e518e8
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-04-02 23:29:36 +0900

    Followモデルにユニーク制約を追加

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,4 +3,6 @@
 class Follow < ApplicationRecord
   belongs_to :followee, class_name: 'User'
   belongs_to :follower, class_name: 'User'
+
+  validates :followee_id, uniqueness: { scope: :follower_id }
 end
