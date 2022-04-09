8e54ae18dfd926bea2b3c1882329bcb742d65035
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-04-09 12:03:50 +0900

    nullの考慮をしなくても良いように、Followテーブルのfollower_idとfollowee_idのnullを許容しないようにした

--- db/migrate/20220330051456_create_follows.rb
+++ db/migrate/20220330051456_create_follows.rb
@@ -3,8 +3,8 @@
 class CreateFollows < ActiveRecord::Migration[6.1]
   def change
     create_table :follows do |t|
-      t.integer :follower_id
-      t.integer :followee_id
+      t.integer :follower_id, null: false
+      t.integer :followee_id, null: false
 
       t.timestamps
       t.index %i[followee_id follower_id], unique: true
