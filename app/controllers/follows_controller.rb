cc8aa4216cabad24f7c5f0af4a20bf6a48915605
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-04-09 11:47:24 +0900

    save だとエラーが検知できないため、save! に変更

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -4,7 +4,7 @@ class FollowsController < ApplicationController
   def create
     followee_id = params[:followee_id]
     @follow = Follow.new(followee_id: followee_id, follower_id: current_user.id)
-    @follow.save
+    @follow.save!
     redirect_to user_path(followee_id)
   end
 
