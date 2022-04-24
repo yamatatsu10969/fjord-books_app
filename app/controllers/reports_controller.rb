86c67e2e5e0efb27298160e5909e72dd0a0e65c6
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-04-24 22:02:54 +0900

    応答がHTMLだけなので respond_to を削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -27,32 +27,26 @@ class ReportsController < ApplicationController
     @report = Report.new(report_params)
     @report.author = current_user
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human) }
-      else
-        format.html { render :new }
-      end
+    if @report.save
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /reports/1
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human) }
-      else
-        format.html { render :edit }
-      end
+    if @report.update(report_params)
+      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit
     end
   end
 
   # DELETE /reports/1
   def destroy
     @report.destroy
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
-    end
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
