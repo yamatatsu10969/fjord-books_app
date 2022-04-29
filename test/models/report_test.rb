af5ab2f899946c99821cf126138d036f0563d130
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-04-29 18:25:08 +0900

    report の unit test を追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,15 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    report = Report.new(user: users(:one))
+    assert report.editable?(users(:one))
+    assert_not report.editable?(users(:two))
+  end
+
+  test '#created_on' do
+    time = Time.zone.now
+    report = Report.new(created_at: time.to_date)
+    assert_equal time.to_date, report.created_on
+  end
 end
