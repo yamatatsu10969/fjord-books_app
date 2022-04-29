39374edfc5dcb6aff6cacadc415a5c78a01d869e
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-04-29 18:46:19 +0900

    User の unit test を追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,47 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @me = User.create!(email: 'me@example.com', password: 'password')
+    @she = User.create!(email: 'she@example.com', password: 'password')
+  end
+
+  teardown do
+    @me = nil
+    @she = nil
+  end
+
+  test '#following?' do
+    assert_not @me.following?(@she)
+    @me.follow(@she)
+    assert @me.following?(@she)
+  end
+
+  test '#followed_by?' do
+    assert_not @she.followed_by?(@me)
+    @she.follow(@me)
+    assert @she.followed_by?(@me)
+  end
+
+  test '#follow' do
+    assert_not @me.following?(@she)
+    @me.follow(@she)
+    assert @me.following?(@she)
+  end
+
+  test '#unfollow' do
+    assert_not @me.following?(@she)
+    @me.follow(@she)
+    assert @me.following?(@she)
+    @me.unfollow(@she)
+    assert_not @me.following?(@she)
+  end
+
+  test '#name_or_email' do
+    @me.name = ''
+    assert_equal 'me@example.com', @me.name_or_email
+
+    @me.name = 'Bob'
+    assert_equal 'Bob', @me.name_or_email
+  end
 end
